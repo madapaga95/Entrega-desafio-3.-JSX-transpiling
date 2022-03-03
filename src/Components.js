42e61314f973547e7a1eb8ff41e169eeceafa1ff
madapaga95 <100244601+madapaga95@users.noreply.github.com>
2022-03-03 20:08:04 -0300

    Entrega desafio 2 clase 3 Maicol Parga

---
+++ src/Components.js
@@ -0,0 +1,12 @@
+function Components (){
+        return <header>
+            <h1>Virtual david,s web</h1>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Registrate</a>
+                <a href="#">Carrito de compras</a>
+            </nav>
+        </header>
+}
+
+export default Components
